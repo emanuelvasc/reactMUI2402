6e83df64a71e52e4e562efed8d99271859fe27c0
FabianaN1 <0001162789@senaimgaluno.com.br>
2026-02-25 14:01:15 -0300

    Atualização do projeto

--- reactMUI/src/App.jsx
+++ reactMUI/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
 import Quantidade from "./Components/Quantidade";
 import FormPreferencias from "./Components/FormPreferencias";
 import FormConfiguracoes from "./Components/FormConfiguracoes";
@@ -11,15 +12,24 @@ function App() {
 
   return (
     <>
+      {/* Logos */}
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
+      {/* 👇 SEUS COMPONENTES (agora aparecem) */}
+      <Quantidade />
+      <FormPreferencias />
+      <FormConfiguracoes />
+
+      {/* Contador padrão */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +38,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
